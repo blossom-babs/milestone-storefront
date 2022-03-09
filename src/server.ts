17de7416007f58cd74cf3f1ef11fe9dfefb4e205
Blossom <saphashb@gmail.com>
2022-03-09 10:16:53 +0100

    project cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,15 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { MythicalWeaponStore } from "./models/mythical_weapon";
-import { Weapon } from './models/mythical_weapon';
-
-
-
-
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
 
 app.use(bodyParser.json());
 
-const mythicalWeapon = new MythicalWeaponStore();
 
 app.get("/", async function (req: Request, res: Response) {
- const weapon: Weapon = {
-     id: 2,
-     name: 'Sword',
-     type: 'Doubled Edged',
-     weight: 50
- }
- const mythicalWeapon = new MythicalWeaponStore();
- const result = await  mythicalWeapon.create(weapon)
-  res.send(result);
-  
+  res.status(200).send('Hello World')
+
 });
 
 
