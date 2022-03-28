7072d7392a0c3e34dc973d25ae1e853cbc802996
Blossom <saphashb@gmail.com>
2022-03-28 12:08:45 +0100

    db-migrations

--- migrations/sqls/20220309111810-books-table-up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE books (
-id SERIAL PRIMARY KEY,
-title VARCHAR(100),
-author VARCHAR(50),
-total_pages INTEGER,
-category VARCHAR(100),
-summary VARCHAR(500)
-)
-
---
+++ migrations/sqls/20220328105228-storefront-up.sql
@@ -0,0 +1,8 @@
+/* Replace with your SQL commands */
+CREATE TABLE product(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  category VARCHAR(255) NOT NULL
+)
+
