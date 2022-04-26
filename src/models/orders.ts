dc6eec32b021d27aa28d9d96e2660415e66e196e
Blossom <saphashb@gmail.com>
2022-04-26 12:09:18 +0100

    update orders model method

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -8,11 +8,35 @@ export type Order = {
 }
 
 export class OrderStore {
-  async create(quantity:number, status:string, userId:number, productId:number) {
+  async index(): Promise<Order> {
+    try {
+      const conn = await client.connect()
+      const sql = `SELECT * FROM orders`
+      const result = await conn.query(sql)
+      conn.release()
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`${error}`)
+    }
+  }
+
+  async create(quantity: number, status: string, userId: number, productId: number): Promise<Order[]> {
     try {
       const conn = await client.connect();
       const sql = `INSERT INTO orders (userId, productId, status, quantity) VALUES ($1, $2, $3, $4) RETURNING *`;
       const result = await conn.query(sql, [userId, productId, status, quantity]);
+      conn.release()
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`${error}`)
+    }
+  }
+
+  async show(id: string): Promise<Order[]> {
+    try {
+      const conn = await client.connect()
+      const sql = `SELECT FROM order WHERE id=${id}`
+      const result = await conn.query(sql)
       conn.release()
       return result.rows[0]
     } catch (error) {
