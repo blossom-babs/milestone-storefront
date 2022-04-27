1c6ed23f6074a4fca956d3cbb230e20efa4906b3
Blossom <saphashb@gmail.com>
2022-04-27 13:44:26 +0100

    edit order migration file

--- migrations/sqls/20220426120705-orders-up.sql
+++ migrations/sqls/20220426120705-orders-up.sql
@@ -1,7 +1,5 @@
 CREATE TABLE orders(
   id SERIAL PRIMARY KEY,
   userId bigint REFERENCES users(id) NOT NULL,
-  productId bigint REFERENCES products(id) NOT NULL,
-  order_status VARCHAR(255) NOT NULL,
-  quantity INTEGER NOT NULL
+  order_status VARCHAR(255) NOT NULL
 )
